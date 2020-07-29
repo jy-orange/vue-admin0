7f180f4e2e6c52f6712ac054885f6d5bc015444f
unknown <1094121954@qq.com>
2020-07-29 11:54:33 +0800

    构建项目、配置全局样式、路由、ElementUI

---
+++ src/views/Login/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div id="login">这是目录页</div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>  // scoped 是为了防止CSS样式对其他文件的影响
+	#login {
+		height: 100vh;   // vh 是基于可视区域的比例来处理的
+		background-color: #344a5f;
+	}
+</style>
